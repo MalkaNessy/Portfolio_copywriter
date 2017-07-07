eef1f32a0bc15ceb9d2533d21d7dc8726a4e9272
Malka <malka.nessy@gmail.com>
2017-07-07 17:06:54 +0300

    update connection, to support bluehost parameters [AviK,Shlomo]

--- connection.php
+++ connection.php
@@ -2,11 +2,12 @@
 
 function dbconnect ()
 {
-	$link = mysql_connect('localhost', 'root', '');
+	$link = mysql_connect('localhost', 'themalka_root', 'oIN0PXW1-;at');
 	if (!$link) {
 		die('Could not connect: ' . mysql_error());
 	}
-	mysql_select_db('writer', $link);
+	mysql_select_db('themalka_writer', $link);
+        mysql_set_charset('utf8',$link);
 	return $link;
 }
 
